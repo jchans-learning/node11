a7f8497c40fa46f4568ca244dbd33c441931c9a5
jchans <jiangchenghans@gmail.com>
2021-01-06 11:54:19 +0800

    add comment

--- src/person.js
+++ src/person.js
@@ -7,12 +7,16 @@ class Person {
 
     // toJSON() ，老師自己定義的
 
+    // 回傳 JSON 
     toJSON(){
         return {
             name: this.name,
             age: this.age,
         }
     }
+
+    // 回傳成為把 JSON 轉為字串
+    //
     // toJSON(){
     //     return JSON.stringify({
     //         name: this.name,
