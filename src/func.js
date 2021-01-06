8a71e024fe81b6715bfe57afd5729620d421acc1
jchans <jiangchenghans@gmail.com>
2021-01-06 10:41:07 +0800

    practice export multiple functions

--- src/func.js
+++ src/func.js
@@ -1,8 +1,8 @@
 // 函式寫法
-// const f1 = function (a) {
-//     return a*a;
-// }
-// console.log('f1(6) = ', f1(6));
+const f1 = function (a) {
+    return a+a;
+}
+console.log('f1(6) = ', f1(6));
 
 // 箭頭函示，括號在只有一個變數的時候可以省略
 // 多個變數的時候括號不可省略
@@ -11,4 +11,4 @@
 const f2 = a => a*a
 console.log('f2(7) = ', f2(7));
 
-module.exports = f2;
+module.exports = {f1, f2};
--- src/function_test.js
+++ src/function_test.js
@@ -1,8 +1,25 @@
 // 第一次 require 的時候會執行一次被 require 的 func
 // 會紀錄被 require 的東西，再次 require 得到的會是相同的東西
 // 所以不會再執行一次
-
+//
 const your_name = require('./func')
 const your_name2 = require('./func')
 
+// 兩次 require 同一個東西，他們是嚴格相等
 console.log(your_name===your_name2);
+
+// 匯出匯入的檔案，如果用 Object 包著多個函式，可以如下使用
+// 也可以用 Array 匯出，但比較常用 Object
+//
+console.log(
+    your_name.f1(10),
+    your_name.f2(20)
+);
+
+// 或者用解構的方式 import
+//
+const {f1: func1, f2}  = require('./func')
+
+// 解構的同時可更換函式名稱
+console.log(func1(33));
+console.log(f2(44));
