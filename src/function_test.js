6a93d4b77df268137ec00daf856fa972d91710f0
jchans <jiangchenghans@gmail.com>
2021-01-06 10:26:14 +0800

    practice module export

--- src/function_test.js
+++ src/function_test.js
@@ -1,1 +1,8 @@
+// 第一次 require 的時候會執行一次被 require 的 func
+// 會紀錄被 require 的東西，再次 require 得到的會是相同的東西
+// 所以不會再執行一次
+
 const your_name = require('./func')
+const your_name2 = require('./func')
+
+console.log(your_name===your_name2);
