172e1d384019e80d4fa0ad68820bed242ccd1fe0
jchans <jiangchenghans@gmail.com>
2021-01-07 10:03:44 +0800

    add utf-8 charset

--- src/http_server1.js
+++ src/http_server1.js
@@ -3,10 +3,10 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, {
-        'Content-Type': 'text/html'
+        'Content-Type': 'text/html; charset=utf8'
     });
     res.end(`
-        <h2>Hello</h2>
+        <h2>Hello 我要說中文！</h2>
         <p>${req.url}
     `);
 });
