12f38162eef059d54176fd4c92d5584de23b53af
jchans <jiangchenghans@gmail.com>
2021-01-07 15:42:02 +0800

    add comments and change port setting

--- src/index.js
+++ src/index.js
@@ -10,11 +10,14 @@ app.use((req, res) => {
     res.type('text/plain');
     res.status(404);
     res.send('404 - 找不到網頁');
+    // 合併寫法
+    // res.status(404).send('404 - 找不到網頁');
 });
 
     // 4 開頭是用戶端的錯誤，這一段要放在程式碼相對較後面的位置
     // 嚴謹的規則放前面，寬鬆的規則放後面，沒有後蓋前的規則。
 
-app.listen(3006, function () {
-    console.log('啟動 server 監聽 PORT 3006');
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> {
+    console.log(`啟動 server 監聽 PORT ${port}`);
 })
