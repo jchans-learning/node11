841ac2ec081f7066fb0520e1512d1da26d593608
jchans <jiangchenghans@gmail.com>
2021-01-07 09:45:31 +0800

    simple web server

---
+++ src/http_server1.js
@@ -0,0 +1,13 @@
+// src/http_server1.js
+
+const http = require('http');
+const server = htto.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    res.end(`
+        <h2>Hello</h2>
+        <p>${req.url}
+    `);
+});
+server.listen(3000);
