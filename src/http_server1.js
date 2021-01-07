bce5ebfa3191b0d9fd5dc1971f2aa057b2d595e1
jchans <jiangchenghans@gmail.com>
2021-01-07 09:46:03 +0800

    fix typo

--- src/http_server1.js
+++ src/http_server1.js
@@ -1,7 +1,7 @@
 // src/http_server1.js
 
 const http = require('http');
-const server = htto.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     res.writeHead(200, {
         'Content-Type': 'text/html'
     });
