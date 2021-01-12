19457fce14aefc2b2bdbf48288e2282c11771bb9
jchans <jiangchenghans@gmail.com>
2021-01-12 16:30:11 +0800

    remove output, simplify the code

--- src/routes/address-book.js
+++ src/routes/address-book.js
@@ -3,21 +3,27 @@ const router = express.Router();
 const db = require(__dirname + '/../modules/db_connect2')
 
 router.get('/list', async (req, res)=>{
-  const output = {
-    perPage: 10,
-  };
-  const [t_rows] = await db.query("SELECT COUNT(1) num FROM `address_book`")
-  output.totalRows = t_rows[0].num;
-  output.totalPages = Math.ceil(output.totalRows/output.perPage)
+  const perPage= 10;
+  const [t_rows] = await db.query("SELECT COUNT(1) num FROM `address_book`");
+  const totalRows = t_rows[0].num;
+  const totalPages = Math.ceil(totalRows/perPage);
 
   let page = parseInt(req.query.page) || 1;
 
   const [rows] = await db.query("SELECT * FROM `address_book` ORDER BY `sid` DESC LIMIT ?, ?", 
-  [(page-1)*output.perPage, output.perPage]);
-  output.rows = rows;
-  res.json(output)
-  //
+  [(page-1)*perPage, perPage]);
+  
+  res.json({
+    perPage,
+    totalRows,
+    totalPages,
+    page,
+    rows,
+  })
+  
   // res.render('address-book/list', output);
+
+  // 如過果要改路由，例如用在 /address-book/ 要怎麼寫？
   
 })
 
