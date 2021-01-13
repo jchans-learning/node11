61355213f9a2bf96b5ed160d1c18d133d66c7fec
jchans <jiangchenghans@gmail.com>
2021-01-13 10:17:03 +0800

    In class note

--- src/routes/address-book.js
+++ src/routes/address-book.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const db = require(__dirname + '/../modules/db_connect2')
 
-router.get('/list', async (req, res)=>{
+// async 本身回傳一個 Promise 物件，非同步的動作，不能沒有 await
+const listHandler = async (req, res)=>{
   const perPage= 10;
   const [t_rows] = await db.query("SELECT COUNT(1) num FROM `address_book`");
   const totalRows = t_rows[0].num;
@@ -12,31 +13,39 @@ router.get('/list', async (req, res)=>{
 
   let page = parseInt(req.query.page) || 1;
   if(totalRows > 0){
-    if(page < 1) {
-      return res.redirect('/address-book/list');
-    }
-    if(page > totalPages) {
-      return res.redirect(
-      `/address-book/list?page=${totalPages}`
-      );
-    };
+    if(page < 1) page = 1;
+    if(page > totalPages) page = totalPages;
 
     [rows] = await db.query("SELECT * FROM `address_book` ORDER BY `sid` DESC LIMIT ?, ?", 
   [(page-1)*perPage, perPage]);
   }
-
-  res.render('address-book/list', {
+  return {
     perPage,
     totalRows,
     totalPages,
     page,
     rows,
-  })
+  }
+};
+
+  // res.render('address-book/list', {
+  //   perPage,
+  //   totalRows,
+  //   totalPages,
+  //   page,
+  //   rows,
+  // })
   
   // res.render('address-book/list', output);
-
   // 如過果要改路由，例如用在 /address-book/ 要怎麼寫？
-  
+
+router.get('/list', async (req, res)=>{
+  const output = await listHandler(req);
+  res.render('address-book/list', output);
+})
+router.get('/api/list', async (req, res)=>{
+  const output = await listHandler(req);
+  res.json(output);
 })
 
 module.exports = router;
